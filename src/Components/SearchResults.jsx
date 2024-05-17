511d2f6faae12f208f5fa120dfafc81c58aba9ae
mrdeepak125 <asus0gl8@outlook.com>
2024-05-17 19:08:57 +0530

    ff

--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 
 function SearchResult({ searchResults, handlePlay }) {
   return (
-    <div className="section">
-      <h1 className="section-title">🔎 Search <span className="text-primary">.</span></h1>
-      <div className="song-list">
-        {searchResults.map((song) => (
-          <div key={song.id} className="song-card" onClick={() => handlePlay(song)}>
-            <img src={song.image} alt={song.title} />
-            <div className="song-info">
-              <h3>{song.song}</h3>
-              <p>{song.singers || 'unknown'}</p>
+    <div className='search-result'>
+        <div className="section">
+        <h1 className="section-title">🔎 Search <span className="text-primary">.</span></h1>
+        <div className="song-list">
+            {searchResults.map((song) => (
+            <div key={song.id} className="song-card" onClick={() => handlePlay(song)}>
+                <img src={song.image} alt={song.title} />
+                <div className="song-info">
+                <h3>{song.song}</h3>
+                <p>{song.singers || 'unknown'}</p>
+                </div>
             </div>
-          </div>
-        ))}
-      </div>
+            ))}
+        </div>
+        </div>
     </div>
   );
 }
