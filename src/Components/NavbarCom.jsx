0f2e015b0d25b372d806a912fccda2a00546a0f6
mrdeepak125 <asus0gl8@outlook.com>
2024-05-19 11:50:43 +0530

    add favicon

--- src/Components/NavbarCom.jsx
+++ src/Components/NavbarCom.jsx
@@ -1,45 +1,45 @@
-import React, { useEffect, useState } from "react";
-import Search from "../assets/img/system-solid-42-search .png";
-import { getSongsByQuery } from "../lib/fetch";
-import { useNavigate } from "react-router-dom";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import Search from '../assets/img/system-solid-42-search .png';
+import { getSongsByQuery } from '../lib/fetch';
+import { useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 function Navbar({ updateSearchResults }) {
   const [darkMode, setDarkMode] = useState(false);
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
 
   const navigate = useNavigate();
 
   useEffect(() => {
-    const isDarkMode = localStorage.getItem("darkMode") === "true";
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
     setDarkMode(isDarkMode);
 
-    document.body.style.backgroundColor = isDarkMode ? "#141619" : "#fff";
-    document.body.style.color = isDarkMode ? "#B3B4BD" : "#141619";
+    document.body.style.backgroundColor = isDarkMode ? '#141619' : '#fff';
+    document.body.style.color = isDarkMode ? '#B3B4BD' : '#141619';
   }, []);
 
   const toggleDarkMode = () => {
     const newDarkMode = !darkMode;
     setDarkMode(newDarkMode);
-    localStorage.setItem("darkMode", newDarkMode);
+    localStorage.setItem('darkMode', newDarkMode);
 
-    document.body.style.backgroundColor = newDarkMode ? "#141619" : "#fff";
-    document.body.style.color = newDarkMode ? "#B3B4BD" : "#141619";
+    document.body.style.backgroundColor = newDarkMode ? '#141619' : '#fff';
+    document.body.style.color = newDarkMode ? '#B3B4BD' : '#141619';
   };
 
   const handleSearch = async () => {
     try {
       const response = await getSongsByQuery(query);
       const data = await response.json();
-      console.log("Search Results:", data);
+      console.log('Search Results:', data);
       updateSearchResults(data);
-      navigate("/search");
+      navigate('/search');
     } catch (error) {
-      console.error("Error fetching search results:", error);
+      console.error('Error fetching search results:', error);
     }
   };
 
   const handleKeyPress = (e) => {
-    if (e.key === "Enter") {
+    if (e.key === 'Enter') {
       handleSearch();
     }
   };
@@ -47,8 +47,8 @@ function Navbar({ updateSearchResults }) {
   return (
     <div className="navbar">
       <div className="left-section">
-        <Link to="/">Home</Link>
-        <Link to="/downloads">Downloads</Link>
+          <Link to="/">Home</Link>
+          <Link to="/downloads">Downloads</Link>
       </div>
       <div className="right-section">
         <input
@@ -67,11 +67,7 @@ function Navbar({ updateSearchResults }) {
         </div>
         <div className="dark-mode">
           <label className="ui-switch">
-            <input
-              type="checkbox"
-              checked={darkMode}
-              onChange={toggleDarkMode}
-            />
+            <input type="checkbox" checked={darkMode} onChange={toggleDarkMode} />
             <div className="slider">
               <div className="circle" />
             </div>
