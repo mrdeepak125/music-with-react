b00ec49bd9c2b8ece89f8ed1e4fdcb316276ba54
mrdeepak125 <asus0gl8@outlook.com>
2024-05-20 20:51:34 +0530

    fix error to install app function

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({
+  onNeedRefresh() {
+    console.log('New content available, please refresh.');
+  },
+  onOfflineReady() {
+    console.log('App is ready to work offline.');
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -8,32 +8,25 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
-        name: 'Your App Name',
-        short_name: 'AppName',
-        description: 'Your app description',
-        theme_color: '#ffffff',
+        name : "Music Location",
+        short_name : "Music",
+        theme_color: "#2563eb",
+        background_color : "#2563eb",
+        start_url : "/",
+        display : "standalone",
+        orientation : "portrait",
         icons: [
-          {
-            src: 'icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'icon-256x256.png',
-            sizes: '256x256',
-            type: 'image/png',
-          },
-          {
-            src: 'icon-384x384.png',
-            sizes: '384x384',
-            type: 'image/png',
-          },
-          {
-            src: 'icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
+            {
+                src: '/icons/icon.png',
+                sizes: '192x192',
+                type: 'image/png'
+              },
+              {
+                src: '/icons/icon(1).png',
+                sizes: '512x512',
+                type: 'image/png'
+              }
+        ]
       },
       workbox: {
         runtimeCaching: [
